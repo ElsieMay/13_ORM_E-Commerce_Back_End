64df503dd3177b11fa440d76e4c9b7cca3832567
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-21 22:43:17 +1000

    added fields and rules for Product and product tag models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		product_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "product_tag",
+	}
 );
 
 module.exports = ProductTag;
