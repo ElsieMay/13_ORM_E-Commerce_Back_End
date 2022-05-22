1637b5aa3463690c6f59c8ac2c84f83d0bccdf35
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-22 15:50:08 +1000

    added get request, added post request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,8 +43,13 @@ router.get("/:id", async (req, res) => {
 	}
 });
 
-router.post("/", (req, res) => {
-	// create a new category
+router.post("/", async (req, res) => {
+	try {
+		const postCategory = await Category.create(req.body);
+		res.status(200).json(postCategory);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.put("/:id", (req, res) => {
