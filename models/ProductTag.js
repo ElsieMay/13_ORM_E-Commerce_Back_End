4fc92c3f8b2fd5a5ef9eabacb468b633705b666b
ElsieMay <elsiemaylawrie1@gmail.com>
2022-05-22 13:42:54 +1000

    added data to product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -12,9 +12,19 @@ ProductTag.init(
 			primaryKey: true,
 			autoIncrement: true,
 		},
-		product_name: {
-			type: DataTypes.STRING,
-			allowNull: false,
+		products_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: "product",
+				key: "id",
+			},
+		},
+		tag_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: "tag",
+				key: "id",
+			},
 		},
 	},
 	{
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -12,7 +12,6 @@ router.get("/", async (req, res) => {
 				{
 					model: Tag,
 					through: ProductTag,
-					as: "category",
 				},
 			],
 		});
